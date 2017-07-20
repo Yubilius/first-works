5893804684b6118a983f91de2058f4367d888551
Yubilius <shinoby.hatake@gmail.com>
2017-07-20 17:46:31 -0500

    Add files via upload

---
+++ HolaMundo.rb
@@ -0,0 +1,14 @@
+class HolaMundo
+    def initialize()
+    end
+
+
+      def suma ()
+        one =1.5
+        two =15
+        puts one+two
+      end
+end
+    objeto = HolaMundo.new()
+objeto.suma
+gets()
